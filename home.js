bbc51a8335433f0364950b0fc57e8b37e9a51e01
danielcn18 <dchiquete2019@gmail.com>
2023-04-15 14:34:56 -0700

    Updated - 4.15.23

--- home.js
+++ home.js
@@ -1,22 +1,20 @@
 const enhanced = id => {
     const element = document.getElementById(id), 
     text = element.innerText.split("");
-    console.log("Hello World");
-
-
     element.innerText = "";
-
     text.forEach(letter => {
         const span = document.createElement("span");
-
-        span.className = "letter";
-
+        span.className += "letter";
+        if(id == "channel-link1"){
+            span.className += " letter1";
+        }
+        if(id == "channel-link2"){
+            span.className += " letter2";
+        }
         span.innerText = letter;
-
         element.appendChild(span);
         console.log(letter);
     });
 }
-
 enhanced("channel-link1");
 enhanced("channel-link2");
